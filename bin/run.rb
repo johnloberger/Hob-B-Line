363f790625371b9364f3a1833318756fa8584c74
skandow <kando1sr@gmail.com>
2020-05-26 17:15:31 -0500

    more run file tests

--- bin/run.rb
+++ bin/run.rb
@@ -11,49 +11,45 @@ puts
 puts
 print "You are a: "
 
-entered_role = gets.chomp
-
-if entered_role == "mentor" || entered_role == "mentee"
-
-  clear_screen!
-  puts "Hello #{ entered_role }! Please enter your full name below!"
-  puts
-  puts
-  print "Full Name: "
-
-  entered_name = gets.chomp
-
-  if entered_role == "mentor"
+while entered_role = gets.chomp
+  case entered_role 
+  when "mentor"
+    break 
+  when "mentee"
+    break
+  else 
     clear_screen!
-    puts "Welcome #{ entered_name }! Here is a list of all available mentees!"
+    puts "Please input a valid role"
     puts
-    puts
-    puts Mentee.all
-
-  elsif entered_role == "mentee"
-    clear_screen!
-    puts "Welcome #{ entered_name }! Here is a list of all available mentors!"
-    puts
-    puts
-    puts Mentor.all
-
-  else
-    puts "Sorry, name was not found. Please re-enter with a valid name!"
-  end
-
-else 
-
-  clear_screen!
-  puts "Please enter a valid role."
-  puts 
-  puts
-  print "You are a: "
-
-  entered_role
-
+    print "You are a:"
+  end 
 end
 
+clear_screen!
+puts "Hello #{ entered_role }! Please enter your full name below!"
+puts
+puts
+print "Full Name: "
 
+entered_name = gets.chomp
 
+if entered_role == "mentor"
+  clear_screen!
+  puts "Welcome #{ entered_name }! Here is a list of all available mentees!"
+  puts
+  puts
+  puts Mentee.all
 
+elsif entered_role == "mentee"
+  clear_screen!
+  puts "Welcome #{ entered_name }! Here is a list of all available mentors!"
+  puts
+  puts
+  puts Mentor.all
+
+else
+  puts "Sorry, name was not found. Please re-enter with a valid name!"
+end
+
+puts
 puts "HELLO WORLD"
